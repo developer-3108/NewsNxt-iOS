feab9ff6f3456a0b62ef14b6c3b402cc07bb3416
developer-3108 <developer.akshat310804@gmail.com>
2025-01-24 17:01:00 +0530

    Initial Commit

---
+++ NewsNxt/NewsNxtApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsNxtApp.swift
+//  NewsNxt
+//
+//  Created by Akshat Srivastava on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsNxtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
